9e71b6580a26bbb209b9b8bccdda956b89ff7f65
Charles Ferringer <charlesferringer@yahoo.com>
2018-10-12 09:45:18 -0400

    commit

--- server/client/src/components/header.js
+++ server/client/src/components/header.js
@@ -2,21 +2,21 @@ import React, {Component} from 'react'
 import { Navbar, NavItem, Icon } from 'react-materialize'
 import './Header.css'
 import DropdownBreed from './DropdownBreed.js'
-import LogoWhite from './LogoWhite.png'
+import NewLogoWhite from './NewLogoWhite.png'
 
 //Header component to be displayed on every page
 
-const Img = <img className="logo" src={LogoWhite}/>
+const Img = <img className="logo" src={NewLogoWhite}/>
 
 class Header extends Component {
 
     render() {
         return (
             <div>
-                <Navbar brand={Img} right>
+                <Navbar brand={Img} right fixed="true">
                     <NavItem href='get-started.html'><Icon>search</Icon></NavItem>
                     <NavItem href='get-started.html'><Icon>view_module</Icon></NavItem>
-                    <NavItem onClick={() => console.log('test click')}>Getting started</NavItem>
+                    <NavItem href='/about'>About</NavItem>
                     <NavItem href='/animals'>Animals</NavItem>
                     <NavItem href='/signup'>Sign Up</NavItem>
                 </Navbar>
